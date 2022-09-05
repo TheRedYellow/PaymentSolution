d5eabc9e6c36fb6d1391ac980806566b5881d2a8
egulsoy <egulsoy@ulusalfaktoring.com>
2022-09-05 10:33:26 +0300

    Added Account_MakeAdjustment_WithSuccess test item

--- Payment.Api.Tests/AccountTest.cs
+++ Payment.Api.Tests/AccountTest.cs
@@ -14,8 +14,7 @@ namespace Payment.Api.Tests
         {
            
            Db db = new Db(
-               new Dictionary<long, decimal>(){{ 1,1000m }}
-               //, new List<Models.Transaction>() { new Models.Transaction() { AccountId=1,TransactionId=Guid.NewGuid().ToString(),Amount=100} }
+               new Dictionary<long, decimal>(){{ 1,1000m }} //Accounts
                );
             AccountService accountService = new AccountService(db);
             accountService.ProcessPayment(1, Guid.NewGuid().ToString(), Models.OriginType.MASTER, 100);
@@ -25,6 +24,25 @@ namespace Payment.Api.Tests
 
             Assert.That(expectedResult.Equals(actualResult));  
            
+        }
+
+        [Test]
+        public void Account_MakeAdjustment_WithSuccess()
+        {
+            string transactionId=Guid.NewGuid().ToString(); 
+
+            Db db = new Db(
+                new Dictionary<long, decimal>() { { 1, 1000m } } //Accounts
+                , new List<Models.Transaction>() { new Models.Transaction() { AccountId=1,TransactionId=transactionId,Amount=100} } //Transactions
+                );
+            AccountService accountService = new AccountService(db);
+            accountService.MakeAdjustment(transactionId, -5);
+
+            decimal expectedResult = 1095m;
+            decimal actualResult = accountService.CheckBalance(1);
+
+            Assert.That(expectedResult.Equals(actualResult));
+
         }
     }
 }
